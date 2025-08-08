92fde0d040298a0e1478dcddccd661572ee5d32a
Clinton Nguyen <clintonnguyen@gmail.com>
2025-08-07 17:42:22 -0700

    parser changes and readme update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# agents
-agent vs agent
+# tompokemontools
+assortment of tools
+
+To get spreadsheet of all replays: `python3 battlelogparser.py` 
+and input the path of the folder with pokemon showdown replays
+(ignore the rows with turn 0 I will fix this later)
+
+
--- battlelogparser.py
+++ battlelogparser.py
@@ -1,17 +1,9 @@
 import pandas as pd
 from bs4 import BeautifulSoup
-import openpyxl
+import os
 
-log_file = input("name of log file: ")
-with open(log_file, "r", encoding="utf-8") as f:
-    html = f.read()
-
-soup = BeautifulSoup(html, 'html.parser')
-script_tag = soup.find('script', {'class':'battle-log-data'})
-log_text = script_tag.string.strip() if script_tag else ""
-
+lengths = []
 turn_data = []
-current_turn = 0
 TOTAL_HEALTH = 341
 
 def eval_health(line):
@@ -21,94 +13,122 @@ def eval_health(line):
     else:
         return nums[0]
 
-current = {
+def parse_battle(html):
+
+    soup = BeautifulSoup(html, 'html.parser')
+    script_tag = soup.find('script', {'class': 'battle-log-data'})
+    log_text = script_tag.string.strip() if script_tag else ""
+    timestamps = []
+
+    current_turn = 0
+
+    current = {
         'p1startmon': '',
         'p1starthealth': 0,
         'p2startmon': '',
         'p2starthealth': 0
-}
+    }
 
-single_turn = {
-            'Turn': current_turn,
-            'P1 Start Pokemon': current['p1startmon'],
-            'P1 Start Health': current['p1starthealth'],
-            'P1 End Pokemon': "",
-            'P1 End Health': "",
-            'P1 Action': '',
-            'P2 Start Pokemon': current['p2startmon'],
-            'P2 Start Health': current['p2starthealth'],
-            'P2 End Pokemon': "",
-            'P2 End Health': "",
-            'P2 Action': ''
-}
+    single_turn = {
+        'Turn': current_turn,
+        'P1 Start Pokemon': current['p1startmon'],
+        'P1 Start Health': current['p1starthealth'],
+        'P1 End Pokemon': "",
+        'P1 End Health': "",
+        'P1 Action': '',
+        'P2 Start Pokemon': current['p2startmon'],
+        'P2 Start Health': current['p2starthealth'],
+        'P2 End Pokemon': "",
+        'P2 End Health': "",
+        'P2 Action': ''
+    }
+    for line in log_text.splitlines():
 
-for line in log_text.splitlines():
+        if '|turn|' in line:
+            turn_data.append(single_turn)
+            current_turn = int(line.split('|turn|')[1])
+            single_turn = {
+                'Turn': current_turn,
+                'P1 Start Pokemon': current['p1startmon'],
+                'P1 Start Health': current['p1starthealth'],
+                'P1 End Pokemon': current['p1startmon'],
+                'P1 End Health': current['p1starthealth'],
+                'P1 Action': 'no action',
+                'P2 Start Pokemon': current['p2startmon'],
+                'P2 Start Health': current['p2starthealth'],
+                'P2 End Pokemon': current['p2startmon'],
+                'P2 End Health': current['p2starthealth'],
+                'P2 Action': 'no action'
+            }
 
-    if '|turn|' in line:
+        elif '|switch|' in line:
+            pokemon = line.split('|')[2].split(':')[1]
+            new_health = eval_health(line)
+            if 'p2a' in line:
+               current['p2startmon'] = pokemon
+               current['p2starthealth'] = new_health
+
+               single_turn['P2 Action'] = f'switch {pokemon}'
+               single_turn['P2 End Pokemon'] = pokemon
+               single_turn['P2 End Health'] = new_health
+
+            else:
+                current['p1startmon'] = pokemon
+                current['p1starthealth'] = new_health
+
+                single_turn['P1 Action'] = f'switch {pokemon}'
+                single_turn['P1 End Pokemon'] = pokemon
+                single_turn['P1 End Health'] = eval_health(line)
+
+
+        elif '|-damage|' in line:
+            if 'p2a' in line:
+                single_turn['P2 End Health'] = eval_health(line)
+                current['p2starthealth'] = single_turn['P2 End Health']
+            else:
+                single_turn['P1 End Health'] = eval_health(line)
+                current['p1starthealth'] = single_turn['P1 End Health']
+
+        elif '|move|' in line:
+            split_line = line.split('|')
+            mover = split_line[2].split(':')[0]
+            move = split_line[3]
+            if mover == 'p2a':
+                single_turn['P2 Action'] = move
+            else:
+                single_turn['P1 Action'] = move
+
+        elif '|t:|' in line:
+            split_line = line.split('|')
+            timestamps.append(int(split_line[2]))
+
+
+    if single_turn:
         turn_data.append(single_turn)
-        current_turn = int(line.split('|turn|')[1])
-        single_turn = {
-            'Turn': current_turn,
-            'P1 Start Pokemon': current['p1startmon'],
-            'P1 Start Health': current['p1starthealth'],
-            'P1 End Pokemon': current['p1startmon'],
-            'P1 End Health': current['p1starthealth'],
-            'P1 Action': '',
-            'P2 Start Pokemon': current['p2startmon'],
-            'P2 Start Health': current['p2starthealth'],
-            'P2 End Pokemon': current['p2startmon'],
-            'P2 End Health': current['p2starthealth'],
-            'P2 Action': ''
-        }
 
-    if '|switch|' in line:
-        pokemon = line.split('|')[2].split(':')[1]
+    df = pd.DataFrame(turn_data)
+    length = timestamps[-1] - timestamps[0] if timestamps else 0
+    return df, length
 
-        if 'p2a' in line:
-           single_turn['P2 Action'] = f'switch {pokemon}'
-           single_turn['P2 End Pokemon'] = pokemon
-           current['p2startmon'] = single_turn['P2 End Pokemon']
-           single_turn['P2 End Health'] = eval_health(line)
-           current['p2starthealth'] = single_turn['P2 End Health']
+# folder_path = input('folder path please: ')
+folder_path = '/Users/clintonnguyen/Downloads/battles'
 
-        else:
-            single_turn['P1 Action'] = f'switch {pokemon}'
-            single_turn['P1 End Pokemon'] = pokemon
-            current['p1startmon'] = single_turn['P1 End Pokemon']
-            single_turn['P1 End Health'] = eval_health(line)
-            current['p1starthealth'] = single_turn['P1 End Health']
+for filename in os.listdir(folder_path):
+    filepath = os.path.join(folder_path, filename)
 
+    with open(filepath, 'r', encoding='utf-8', errors='replace') as f:
+        html = f.read()
 
-    elif '|-damage|' in line:
-        if 'p2a' in line:
-            single_turn['P2 End Health'] = eval_health(line)
-            current['p2starthealth'] = single_turn['P2 End Health']
-        else:
-            single_turn['P1 End Health'] = eval_health(line)
-            current['p1starthealth'] = single_turn['P1 End Health']
-
-    elif '|move|' in line:
-        split_line = line.split('|')
-        mover = split_line[2].split(':')[0]
-        move = split_line[3]
-        if mover == 'p2a':
-            single_turn['P2 Action'] = move
-        else:
-            single_turn['P1 Action'] = move
-
-if single_turn:
-    turn_data.append(single_turn)
-
-df = pd.DataFrame(turn_data)
-df.to_excel('battle_log.xlsx', index=False)
-print('Finished!')
+    df, battle_length = parse_battle(html)
+    df.to_excel('battle_log.xlsx', index=False)
+    print(f'Finished! {filename} took: {battle_length} seconds')
+    lengths.append(abs(battle_length))
 
 
 
-    # previous turn's end becomes next turn's start - handled
-    # move: changes ACTION - handled
-    # switch: changes END POKEMON and ACTION - handled
-    # -damage: changes END HEALTH - handled
-    # when do we want to write to spreadsheet? - handled
-        # probably when we see the next turn
+
+
+
+
+
 
